2327e5b6034e4cdc3d66c2cacc1891f25dc7c629
Luis Zubia <soyzubia@gmail.com>
2023-01-26 21:52:49 -0600

    Add a NavBar component to the App.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 import BooksPage from './pages/BooksPage';
 import CategoriesPage from './pages/CategoriesPage';
+import NavBar from './components/NavBar';
 
 const App = () => (
   <main>
+    <NavBar />
     <h1>Welcome to BooksLog!</h1>
     <Routes>
       <Route index element={<BooksPage />} />
