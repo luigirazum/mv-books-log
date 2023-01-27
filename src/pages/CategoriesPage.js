d42f3be0db194b52264659bd1ee77f4ff6a1737b
Luis Zubia <soyzubia@gmail.com>
2023-01-26 23:38:42 -0600

    Add the Button for Check Status on Categories Page.

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Button from '../components/Button';
 
 const CategoriesPage = () => (
   <div>
-    <h2>Categories Page</h2>
+    <h2>Book&apos;s Categories</h2>
+    <p>Is unavailable right now.</p>
+    <Button
+      buttonType="button"
+      text="Check Status"
+      buttonClass="checkStatus"
+    />
   </div>
 );
 
