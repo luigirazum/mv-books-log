4186d0dc084cea2b928ce333109e5bb091ef00dd
Luis Zubia <soyzubia@gmail.com>
2023-01-26 22:31:49 -0600

    Create the Header component which is composed by AppTitle, NavBar and Avatar.

---
+++ src/components/AppTitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const AppTitle = () => (
+  <nav>
+    <Link to="/" end="true">BooksLog</Link>
+  </nav>
+);
+
+export default AppTitle;
---
+++ src/components/Avatar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaUser } from 'react-icons/fa';
+
+const Avatar = () => (
+  <nav>
+    <button type="button">
+      <FaUser />
+    </button>
+  </nav>
+);
+
+export default Avatar;
