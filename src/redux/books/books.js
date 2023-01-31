cf979edf85deebd2421c15b5298b2f6064465cbb
Luis Zubia <soyzubia@gmail.com>
2023-01-31 16:35:52 -0600

    Add BOOK object to initialState for BOOKS reducer.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -5,9 +5,20 @@ const ADD_BOOK = 'bookslog/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookslog/books/REMOVE_BOOK';
 
 /**
+ * initialState for BOOKS
+ */
+const initialState = [
+  { id: 1, title: 'Book One', author: 'First Author' },
+  { id: 2, title: 'Book Two', author: 'Second Author' },
+  { id: 3, title: 'Book Three', author: 'Third Author' },
+  { id: 4, title: 'Book Four', author: 'Fourth Author' },
+  { id: 5, title: 'Book Five', author: 'Fifth Author' },
+];
+
+/**
  * reducer for BOOKS
  */
-const reducer = (state = [], action = {}) => {
+const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case ADD_BOOK:
       return [
