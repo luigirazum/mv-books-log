c603e5bafffa76759a453c605ca00d5da1038724
Luis Zubia <soyzubia@gmail.com>
2023-01-31 10:44:50 -0600

    Add reducer for BOOKs.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -5,6 +5,28 @@ const ADD_BOOK = 'bookslog/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookslog/books/REMOVE_BOOK';
 
 /**
+ * reducer for BOOKS
+ */
+const reducer = (state = [], action = {}) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          id: state.length + 1,
+          ...action.book,
+        },
+      ];
+    case REMOVE_BOOK:
+      return state.filter(
+        (book) => book.id !== action.id,
+      );
+    default:
+      return state;
+  }
+};
+
+/**
  * action creators for BOOKS
  */
 const addBook = () => ({ type: ADD_BOOK });
@@ -12,3 +34,4 @@ const addBook = () => ({ type: ADD_BOOK });
 const removeBook = () => ({ type: REMOVE_BOOK });
 
 export { addBook, removeBook };
+export default reducer;
