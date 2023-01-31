4bfc7f486ed957d574ecf52d747b1f6abddcf973
Luis Zubia <soyzubia@gmail.com>
2023-01-31 10:54:58 -0600

    Add action types and action creators for CATEGORIES.

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,11 @@
+/**
+ * action types for CATEGORIES
+ */
+const CHECK_STATUS = 'bookslog/categories/CHECK_STATUS';
+
+/**
+ * action creators for CATEGORIES
+ */
+const checkStatus = () => ({ type: CHECK_STATUS });
+
+export { checkStatus };
