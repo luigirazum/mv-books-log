a1816957323563de060e65bfc99ee4fc574a5382
Luis Zubia <soyzubia@gmail.com>
2023-01-31 10:59:16 -0600

    Add reducer for CATEGORIES.

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -4,8 +4,21 @@
 const CHECK_STATUS = 'bookslog/categories/CHECK_STATUS';
 
 /**
+ * reducer for CATEGORIES
+ */
+const reducer = (state = [], action = {}) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under construction';
+    default:
+      return state;
+  }
+};
+
+/**
  * action creators for CATEGORIES
  */
 const checkStatus = () => ({ type: CHECK_STATUS });
 
 export { checkStatus };
+export default reducer;
