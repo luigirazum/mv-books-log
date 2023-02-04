de33c29ea8df35c5017eeb1121005af7a12069e5
Luis Zubia <soyzubia@gmail.com>
2023-02-03 22:19:18 -0600

    Import fetchBooks function to retrieve the books from the API when loading the App.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/storeConfig';
 import App from './App';
+import { fetchBooks } from './redux/books/books';
 import reportWebVitals from './reportWebVitals';
+
+store.dispatch(fetchBooks());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
