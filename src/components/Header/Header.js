743aa1e9d8fa6e2e9dd2f72c0c8dfdafe08eb8ce
Luis Zubia <soyzubia@gmail.com>
2023-02-09 18:36:20 -0600

    Move Header component into its own folder, Add css 'module' styles.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+import './Header.css';
+
+const Header = () => (
+  <header>
+    <NavBar />
+  </header>
+);
+
+export default Header;
